a3ffac6c9b793a0cd7ac3bdd2da0c34a285b3eee
sina <siyam1990@gmail.com>
2024-12-11 18:50:22 +0330

    create productlist page and add it to routhes in app.js

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import UserDetailsPage from "./pages/UserDetails";
 import NotFound from "./pages/NotFound";
+import ProductsList from "./pages/ProductsList";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,8 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/user-details" element={<UserDetailsPage />} />
+          <Route path="/products-list" element={<ProductsList />} />
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
---
+++ src/pages/ProductsList.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ProductsList: React.FC = () => {
+  return <div>ProductsList</div>;
+};
+export default ProductsList;
