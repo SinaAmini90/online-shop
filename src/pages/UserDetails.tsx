8da777aff28dd5d7ff76e3d214a3a4b62df8c827
sina <siyam1990@gmail.com>
2024-12-11 13:09:10 +0330

    add routh to project with home , 404error and userdetail pages

---
+++ src/pages/UserDetails.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserDetails from "../components/UserDetail";
+
+const UserDetailsPage: React.FC = () => {
+  return <UserDetails />;
+};
+
+export default UserDetailsPage;
