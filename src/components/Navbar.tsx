81d4dbd9930af14e11f51ece6537e0946e522327
sina <siyam1990@gmail.com>
2024-12-15 11:54:47 +0330

    add hamberger menue for navigations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
@@ -11,16 +11,18 @@ const Navbar: React.FC = () => {
     (state: RootState) => state.profile.user?.name.firstname || "Guest"
   );
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav className="flex justify-between align-middle p-4 bg-gray-800 text-white">
-      <NavLink to="/user-details" className="my-auto w-3/12">
+    <nav className="flex justify-between items-center p-4 bg-gray-800 text-white relative">
+      <NavLink to="/user-details" className="my-auto w-4/12">
         <p className="text-center border-2 rounded-full w-8 h-8 mr-2 inline-flex justify-center items-center">
           <i className="fa fa-user"></i>
         </p>
-        hi, {profileName}
+        Hi, {profileName}
       </NavLink>
       <p
-        className="text-5xl font-bold w-6/12 text-center "
+        className="text-5xl font-bold w-4/12 text-center"
         style={{
           fontFamily: "'Pacifico', cursive",
           background: "linear-gradient(to right, #ffff5f, #feb47b)",
@@ -31,11 +33,11 @@ const Navbar: React.FC = () => {
         online shop
       </p>
 
-      <div className="my-auto space-x-2 w-3/12 text-right">
+      <div className="hidden md:inline my-auto space-x-4 w-4/12 text-right">
         <NavLink
           className={({ isActive }) =>
             isActive
-              ? "text-orange-300 font-bold "
+              ? "text-orange-300 font-bold"
               : "text-white hover:text-orange-300"
           }
           to="/cart"
@@ -45,17 +47,17 @@ const Navbar: React.FC = () => {
         <NavLink
           className={({ isActive }) =>
             isActive
-              ? "text-orange-300 font-bold "
+              ? "text-orange-300 font-bold"
               : "text-white hover:text-orange-300"
           }
           to="/products-list"
         >
-          products
+          Products
         </NavLink>
         <NavLink
           className={({ isActive }) =>
             isActive
-              ? "text-orange-300 font-bold "
+              ? "text-orange-300 font-bold"
               : "text-white hover:text-orange-300"
           }
           to="/"
@@ -63,6 +65,56 @@ const Navbar: React.FC = () => {
           Home
         </NavLink>
       </div>
+
+      <div className="md:hidden my-auto w-4/12 text-right">
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="text-white focus:outline-none"
+        >
+          <i className="fa fa-bars text-xl"></i>
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+        <div className="absolute top-full right-0 w-full bg-gray-700 md:hidden">
+          <ul className="flex flex-col items-center p-4 space-y-4">
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-300 font-bold"
+                  : "text-white hover:text-orange-300"
+              }
+              to="/cart"
+              onClick={() => setIsMenuOpen(false)} // Close menu on click
+            >
+              ({cartItemsCount}) Cart
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-300 font-bold"
+                  : "text-white hover:text-orange-300"
+              }
+              to="/products-list"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Products
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-300 font-bold"
+                  : "text-white hover:text-orange-300"
+              }
+              to="/"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Home
+            </NavLink>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
