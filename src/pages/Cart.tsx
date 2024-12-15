bc3de02d4a8eb2e04facb69485d8da5413537a0d
sina <siyam1990@gmail.com>
2024-12-15 18:45:45 +0330

    add navigate button to cart ehn its empty and some style

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -2,22 +2,34 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import CartItem from "../components/CartItem";
+import { useNavigate } from "react-router-dom";
 
 const CartPage: React.FC = () => {
   const cartItems = useSelector((state: RootState) => state.cart.items);
-
   const totalPrice = cartItems.reduce(
     (total, item) => total + item.price * item.quantity,
     0
   );
+  const navigate = useNavigate();
+  const handleClickButton = () => {
+    navigate("/products-list");
+  };
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
       {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
+        <div className="flex flex-col items-center justify-center p-4 bg-orange-100 rounded-lg shadow-md w-full sm:w-1/2 mx-auto my-4">
+          <h1>Your cart is empty.</h1>
+          <button
+            onClick={handleClickButton}
+            className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600 transition duration-300"
+          >
+            Start Shoping Now!
+          </button>
+        </div>
       ) : (
         <div>
+          <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
           <table className="w-full border-collapse">
             <thead>
               <tr className="border-b bg-zinc-300">
