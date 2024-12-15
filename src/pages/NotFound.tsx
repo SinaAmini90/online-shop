c28a8ae71046f55bcfaf1b744d05ceeaa7d7d9ff
sina <siyam1990@gmail.com>
2024-12-15 18:46:06 +0330

    add error component to notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import ErrorModule from "../components/Error";
+import { useNavigate } from "react-router-dom";
 
 const NotFound: React.FC = () => {
-  return <h1>404 - Page Not Found</h1>;
+  const navigate = useNavigate();
+  const handleClickButton = () => {
+    navigate("/");
+  };
+  return (
+    <ErrorModule message="404 Error, Page not found ">
+      <button
+        onClick={handleClickButton}
+        className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600 transition duration-300"
+      >
+        Go Home Page
+      </button>
+    </ErrorModule>
+  );
 };
 
 export default NotFound;
