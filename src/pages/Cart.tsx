8729acdfb96ef694895820257b9eb822002d8d90
sina <siyam1990@gmail.com>
2024-12-23 19:07:23 +0330

    fix cart display: add minmum width to get horizentall scroll in small screens

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -30,30 +30,30 @@ const CartPage: React.FC = () => {
       ) : (
         <div>
           <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
-          <table className="w-full border-collapse">
-            <thead>
-              <tr className="border-b bg-zinc-300">
-                <th className="py-2 w-[5%] hidden md:table-cell">#</th>
-                <th className="py-2 w-[25%]">Title</th>
-                <th className="py-2 w-[35%] hidden md:table-cell">
-                  Description
-                </th>
-                <th className="py-2 w-[1%]"></th>
-                <th className="py-2 w-[1%]">Qty</th>
-                <th className="py-2 w-[1%]"></th>
-                <th className="py-2 w-[10%]">Price</th>
-                <th className="py-2 w-[10%]">Total</th>
-                <th className="py-2 w-[5%]"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <CartItem />
-            </tbody>
-          </table>
-          <div className="mt-4 text-right">
-            <h2 className="text-xl font-bold">
-              Total: ${totalPrice.toFixed(2)}
-            </h2>
+          <div className="overflow-x-auto">
+            <table className="w-full min-w-[800px] border-collapse">
+              <thead>
+                <tr className="border-b bg-zinc-300">
+                  <th className="py-2 w-[5%]">#</th>
+                  <th className="py-2 w-[10%]">Title</th>
+                  <th className="py-2 w-[35%]">Description</th>
+                  <th className="py-2 w-[1%]"></th>
+                  <th className="py-2 w-[1%]">Qty</th>
+                  <th className="py-2 w-[1%]"></th>
+                  <th className="py-2 w-[10%]">Price</th>
+                  <th className="py-2 w-[10%]">Total</th>
+                  <th className="py-2 w-[5%]"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <CartItem />
+              </tbody>
+            </table>
+            <div className="mt-4 text-right">
+              <h2 className="text-xl font-bold">
+                Total: ${totalPrice.toFixed(2)}
+              </h2>
+            </div>
           </div>
         </div>
       )}
